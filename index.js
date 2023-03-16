812d5f0fc75f92b1cc929e30d02300c2834419ef
Radek <105140575+RadSum@users.noreply.github.com>
2023-03-16 20:48:32 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,57 +1,81 @@
-const ELEMENT_SIZE = 10;
+const SIZE = 10;
 const counter = document.getElementById("score-counter");
 const canvas = document.getElementById("my-canvas");
 const context = canvas.getContext("2d");
+const highscoreOutput = document.getElementById("highscore-output");
 let score = 0;
+
+highscoreOutput.textContent = `Current highscore is ${localStorage.getItem("highscore")}`;
 
 class SnakePart {
     constructor(px, py) {
         this.px = px;
         this.py = py;
-        this.size = ELEMENT_SIZE;
+        this.size = SIZE;
         this.prevPosX = px;
         this.prevPosX = py;
-    }
-
-    checkBorderCollision() {
-        if (this.px < 0 || this.px > (canvas.width - ELEMENT_SIZE)) {
-            return true;
-        } else if (this.py < 0 || this.py > (canvas.height - ELEMENT_SIZE)) {
-            return true;
-        }
-        return false;
     }
 
     updatePrevPositions() {
         this.prevPosX = this.px;
         this.prevPosY = this.py;
     }
+}
+
+class SnakeHead extends SnakePart {
+    constructor(px, py) {
+        super(px, py);
+    }
+
+    checkBorderCollision() {
+        if (this.px < 0 || this.px > (canvas.width - SIZE)) {
+            return true;
+        } else if (this.py < 0 || this.py > (canvas.height - SIZE)) {
+            return true;
+        }
+        return false;
+    }
 
     checkFoodCollision() {
         if (this.px == foodPosition[0] && this.py == foodPosition[1]) {
             return true;
         }
+        return false;
+    }
+
+    checkSelfCollision() {
+        let res = false;
+        snakeArray.forEach((snakePart, index) => {
+        if (this.px == snakePart.px && this.py == snakePart.py && index != 0 && index != 1) {
+                res = true;
+            }
+        });
+        return res;
     }
 }
 
 let direction = "right";
 document.addEventListener("keydown", event => {
     switch(event.key) {
+        case "ArrowUp":
         case "w":
             if (direction != "down") {
                 direction = "up";
             }
             break;
+        case "ArrowLeft":
         case "a":
             if (direction != "right") {
                 direction = "left";
             }
             break;
+        case "ArrowDown":
         case "s":
             if (direction != "up") {
                 direction = "down";
             }
             break;
+        case "ArrowRight":
         case "d":
             if (direction != "left") {
                 direction = "right";
@@ -62,7 +86,7 @@ document.addEventListener("keydown", event => {
     }
 });
 
-const snakeHead = new SnakePart(50, 20);
+const snakeHead = new SnakeHead(Math.floor(canvas.width/5), Math.floor(canvas.height/2));
 const snakeArray = [snakeHead];
 let foodPosition = [100, 100];
 
@@ -74,43 +98,42 @@ const interval = setInterval(() => {
         drawSnakePart(snakePart);
 
         snakePart.updatePrevPositions();
-        let foodCollide = false;
-        let wallCollide = false;
         if (index == 0) {
-            foodCollide = snakePart.checkFoodCollision();
-            wallCollide = snakePart.checkBorderCollision();
-
             switch(direction) {
                 case "right":
-                    snakePart.px += 10;
+                    snakePart.px += SIZE;
                     break;
                 case "left":
-                    snakePart.px -= 10;
+                    snakePart.px -= SIZE;
                     break;
                 case "up":
-                    snakePart.py -= 10;
+                    snakePart.py -= SIZE;
                     break;
                 case "down":
-                    snakePart.py += 10;
+                    snakePart.py += SIZE;
                     break;
+            }
+
+            if (snakePart.checkFoodCollision()) {
+                foodPosition = [getRandomNumber(canvas.width), getRandomNumber(canvas.height)];
+                drawFood(foodPosition);
+                appendSnake();
+                score++;
+                counter.textContent = score;
+            }
+    
+            if (snakePart.checkBorderCollision() || snakePart.checkSelfCollision()) {
+                counter.textContent = `You lost. Your score was ${score}`;
+                clearInterval(interval);
+                const highscore = localStorage.getItem("highscore");
+                if (highscore < score) {
+                    localStorage.setItem("highscore", score);
+                }
             }
         } else {
             const {prevPosX: px, prevPosY: py} = snakeArray[index - 1];
             snakePart.px = px;
             snakePart.py = py;
-        }
-
-        if (foodCollide) {
-            foodPosition = [Math.floor(Math.random() * (canvas.width/10)) * 10, Math.floor(Math.random() * (canvas.height/10)) * 10];
-            drawFood(foodPosition);
-            appendSnake();
-            score++;
-            counter.textContent = score;
-        }
-
-        if (wallCollide) {
-            counter.textContent = `You lost. Your score was ${score}`;
-            clearInterval(interval);
         }
     });
 }, 75);
@@ -124,11 +147,15 @@ function drawSnakePart(snakePart) {
 
 function drawFood(foodPos) {
     context.fillStyle = "#FF0000";
-    context.fillRect(foodPos[0], foodPos[1], ELEMENT_SIZE, ELEMENT_SIZE);
+    context.fillRect(foodPos[0], foodPos[1], SIZE, SIZE);
 }
 
 function appendSnake() {
     const {px, py} = snakeArray[snakeArray.length - 1];
     const newSnake = new SnakePart(px, py);
     snakeArray.push(newSnake);
+}
+
+function getRandomNumber(bounds) {
+    return Math.floor(Math.random() * (bounds/SIZE)) * SIZE;
 }
