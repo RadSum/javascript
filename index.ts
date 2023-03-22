117de81aad9f30a63ee2a0be662c20cf14b1b00d
Radek <105140575+RadSum@users.noreply.github.com>
2023-03-22 18:54:00 +0100

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,17 +1,26 @@
 const SIZE = 10;
 const counter = document.getElementById("score-counter");
-const canvas = <HTMLCanvasElement> document.getElementById("my-canvas");
+const canvas = <HTMLCanvasElement>document.getElementById("my-canvas");
 const context = canvas.getContext("2d");
 const restartButton = document.getElementById("restart-button");
 const highscoreOutput = document.getElementById("highscore-output");
+const gamemodeSelection = <HTMLSelectElement>(
+    document.getElementById("gamemode-selection")
+);
 let score = 0;
 
-highscoreOutput.textContent = `Current highscore is ${localStorage.getItem("highscore")}`;
+gamemodeSelection.onchange = () => {
+    gamemode = <Mode>gamemodeSelection.value;
+};
+highscoreOutput.textContent = `Current highscore is ${localStorage.getItem(
+    "highscore"
+)}`;
 restartButton.onclick = resetGame;
 restartButton.hidden = true;
 
 type Direction = "right" | "left" | "up" | "down" | "stop";
 type FoodPosition = [number, number];
+type Mode = "normal" | "moreFood";
 
 class SnakePart {
     px: number;
@@ -25,7 +34,7 @@ class SnakePart {
         this.py = py;
         this.size = SIZE;
         this.prevPosX = px;
-        this.prevPosX = py;
+        this.prevPosY = py;
     }
 
     updatePrevPositions() {
@@ -40,25 +49,33 @@ class SnakeHead extends SnakePart {
     }
 
     checkBorderCollision() {
-        if (this.px < 0 || this.px > (canvas.width - SIZE)) {
+        if (this.px < 0 || this.px > canvas.width - SIZE) {
             return true;
-        } else if (this.py < 0 || this.py > (canvas.height - SIZE)) {
+        } else if (this.py < 0 || this.py > canvas.height - SIZE) {
             return true;
         }
         return false;
     }
 
     checkFoodCollision() {
-        if (this.px == foodPosition[0] && this.py == foodPosition[1]) {
-            return true;
-        }
-        return false;
+        let res = false;
+        foodPosition.forEach((foodPos) => {
+            if (this.px == foodPos[0] && this.py == foodPos[1]) {
+                res = true;
+            }
+        });
+        return res;
     }
 
     checkSelfCollision() {
         let res = false;
         snakeArray.forEach((snakePart, index) => {
-        if (this.px == snakePart.px && this.py == snakePart.py && index != 0 && index != 1) {
+            if (
+                this.px == snakePart.px &&
+                this.py == snakePart.py &&
+                index != 0 &&
+                index != 1
+            ) {
                 res = true;
             }
         });
@@ -66,30 +83,38 @@ class SnakeHead extends SnakePart {
     }
 }
 
-document.addEventListener("keydown", event => {
-    switch(event.key) {
+document.addEventListener("keydown", (event) => {
+    switch (event.key.toLowerCase()) {
         case "ArrowUp":
         case "w":
             if (direction != "down" && direction !== "stop") {
-                direction = "up";
+                setTimeout(() => {
+                    direction = "up";
+                }, 50);
             }
             break;
         case "ArrowLeft":
         case "a":
             if (direction != "right" && direction !== "stop") {
-                direction = "left";
+                setTimeout(() => {
+                    direction = "left";
+                }, 50);
             }
             break;
         case "ArrowDown":
         case "s":
             if (direction != "up" && direction !== "stop") {
-                direction = "down";
+                setTimeout(() => {
+                    direction = "down";
+                }, 50);
             }
             break;
         case "ArrowRight":
         case "d":
             if (direction != "left" && direction !== "stop") {
-                direction = "right";
+                setTimeout(() => {
+                    direction = "right";
+                }, 50);
             }
             break;
         case " ":
@@ -107,22 +132,26 @@ document.addEventListener("keydown", event => {
 
 let direction: Direction = "right";
 let lastDirection: Direction;
-let snakeHead = new SnakeHead(Math.floor(canvas.width/5), Math.floor(canvas.height/2));
+let snakeHead = new SnakeHead(
+    Math.floor(canvas.width / 5),
+    Math.floor(canvas.height / 2)
+);
 let snakeArray: SnakePart[] = [snakeHead];
-let foodPosition: FoodPosition = [100, 100];
+let foodPosition: FoodPosition[] = [[100, 100]];
+let gamemode: Mode = <Mode>gamemodeSelection.value;
 
 let mainInterval = setInterval(snakeGame, 75);
 
 function snakeGame() {
     context.clearRect(0, 0, canvas.width, canvas.height);
     drawFood(foodPosition);
-    
+
     snakeArray.forEach((snakePart, index) => {
         drawSnakePart(snakePart);
 
         snakePart.updatePrevPositions();
         if (snakePart instanceof SnakeHead) {
-            switch(direction) {
+            switch (direction) {
                 case "right":
                     snakePart.px += SIZE;
                     break;
@@ -140,26 +169,41 @@ function snakeGame() {
             }
 
             if (snakePart.checkFoodCollision()) {
-                foodPosition = [getRandomNumber(canvas.width), getRandomNumber(canvas.height)];
+                if (gamemode == "moreFood") {
+                    foodPosition.shift();
+                    for (let i = 0; i < 2; i++) {
+                        foodPosition.push([
+                            getRandomNumber(canvas.width),
+                            getRandomNumber(canvas.height),
+                        ]);
+                    }
+                } else {
+                    foodPosition = [
+                        [getRandomNumber(400), getRandomNumber(400)],
+                    ];
+                }
                 drawFood(foodPosition);
                 appendSnake();
                 score++;
                 counter.textContent = String(score);
             }
-    
-            if (snakePart.checkBorderCollision() || snakePart.checkSelfCollision()) {
+
+            if (
+                snakePart.checkBorderCollision() ||
+                snakePart.checkSelfCollision()
+            ) {
                 counter.textContent = `You lost. Your score was ${score}`;
                 restartButton.hidden = false;
                 clearInterval(mainInterval);
                 const highscore = localStorage.getItem("highscore");
                 if (+highscore < score) {
                     localStorage.setItem("highscore", String(score));
+                    highscoreOutput.textContent = `Current highscore is ${score}`;
                 }
             }
-            
         } else {
             if (direction === "stop") return;
-            const {prevPosX: px, prevPosY: py} = snakeArray[index - 1];
+            const { prevPosX: px, prevPosY: py } = snakeArray[index - 1];
             snakePart.px = px;
             snakePart.py = py;
         }
@@ -167,12 +211,16 @@ function snakeGame() {
 }
 
 function resetGame() {
+    gamemode = <Mode>gamemodeSelection.value;
     restartButton.hidden = true;
     score = 0;
     counter.textContent = String(score);
-    snakeHead = new SnakeHead(Math.floor(canvas.width/5), Math.floor(canvas.height/2)); 
+    snakeHead = new SnakeHead(
+        Math.floor(canvas.width / 5),
+        Math.floor(canvas.height / 2)
+    );
     snakeArray = [snakeHead];
-    foodPosition = [100, 100];
+    foodPosition = [[100, 100]];
     direction = "right";
     lastDirection = undefined;
 
@@ -181,22 +229,25 @@ function resetGame() {
 
 function drawSnakePart(snakePart: SnakePart) {
     context.fillStyle = "#000000";
-    const {px, py, size} = snakePart;
+    const { px, py, size } = snakePart;
 
     context.fillRect(px, py, size, size);
 }
 
-function drawFood(foodPos: FoodPosition) {
+function drawFood(foodPos: FoodPosition[]) {
     context.fillStyle = "#FF0000";
-    context.fillRect(foodPos[0], foodPos[1], SIZE, SIZE);
+    for (const [posX, posY] of foodPos) {
+        context.fillRect(posX, posY, SIZE, SIZE);
+    }
 }
 
 function appendSnake() {
-    const {px, py} = snakeArray[snakeArray.length - 1];
+    const { px, py } = snakeArray[snakeArray.length - 1];
     const newSnake = new SnakePart(px, py);
     snakeArray.push(newSnake);
 }
 
 function getRandomNumber(bounds: number) {
-    return Math.floor(Math.random() * (bounds/SIZE)) * SIZE;
+    return Math.floor(Math.random() * (bounds / SIZE)) * SIZE;
 }
+
