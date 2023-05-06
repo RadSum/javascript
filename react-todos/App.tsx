e7e42fba09557823fa7ca48ed7d29c50e58f4d2a
Radek <105140575+RadSum@users.noreply.github.com>
2023-05-06 13:52:18 +0200

    Update App.tsx

--- react-todos/App.tsx
+++ react-todos/App.tsx
@@ -1,40 +1,135 @@
-import './App.css';
-import Todo from './components/Todo.tsx';
-import { useState, useRef, MutableRefObject } from "react";
+import { useState, useEffect } from "react";
+import NewTodoForm from "./Components/NewTodoForm.tsx";
+import TodoList from "./Components/TodoList.tsx";
+import TodoHistory from "./Components/TodoHistory.tsx";
+import TodoButtons from "./Components/TodoButtons.tsx";
+import "./App.css";
+
+export interface Todo {
+  id: string;
+  title: string;
+  completed: boolean;
+}
 
 function App() {
-  const [todos, setTodos] = useState([] as string[]);
-  const input = useRef() as MutableRefObject<HTMLInputElement>;
+  const [todos, setTodos] = useState<Todo[]>(retrieveItem("ITEMS"));
+  const [todoHistory, setTodoHistory] = useState<Todo[]>(retrieveItem("TODO_HISTORY"));
+  const [allChecked, setAllChecked] = useState(false);
+  const [showHistory, setShowHistory] = useState(false);
 
-  function onAdd(text: string) {
-    const newTodos = [...todos, text];
-    setTodos(newTodos);
+  useEffect(() => {
+    localStorage.setItem("ITEMS", JSON.stringify(todos));
+  }, [todos]);
+
+  useEffect(() => {
+    localStorage.setItem("TODO_HISTORY", JSON.stringify(todoHistory));
+  }, [todoHistory])
+
+  function retrieveItem(itemName: string) {
+    const localValue = localStorage.getItem(itemName);
+    if (localValue == null || localValue === "undefined") return [];
+
+    return JSON.parse(localValue);
   }
 
-  function removeTodo(todoText: string) {
-    const todoIdx = todos.indexOf(todoText);
-    todos.splice(todoIdx, 1);
-
-    setTodos([...todos]);
+  function getRandomId() {
+    return Math.random().toString(36).slice(2, 7);
   }
 
-  function handleClick() {
-    const todo = input.current.value;
+  function addTodo(title: string) {
+    setTodos([...todos, { id: getRandomId(), title, completed: false }]);
+  }
 
-    if (todo.trim() !== "") {
-      onAdd(todo);
-      input.current.value = "";
-    }
+  function toggleTodo(id: string, completed: boolean) {
+    setTodos(currentTodos => {
+      return currentTodos.map(todo => {
+        if (todo.id === id) {
+          return { ...todo, completed };
+        }
+
+        return todo;
+      });
+    });
+  }
+
+  function deleteTodo(id: string) {
+    setTodos(currentTodos => {
+      return currentTodos.filter(todo => todo.id !== id);
+    });
+
+    setTodoHistory(currentHistory => {
+      return [...currentHistory, todos.find(todo => todo.id === id) as Todo];
+    });
+  }
+
+  function deleteMarkedTodos() {
+    setAllChecked(false);
+    setTodos(currentTodos => {
+      return currentTodos.filter(todo => !todo.completed);
+    });
+
+    setTodoHistory(currentHistory => {
+      const deletedTodos = todos.filter(todo => todo.completed);
+
+      return [...currentHistory, ...deletedTodos];
+    })
+  }
+
+  function checkAllTodos() {
+    if (todos.length === 0) return;
+
+    setTodos(currentTodos => {
+      return currentTodos.map(todo => {
+        return { ...todo, completed: !allChecked };
+      });
+    });
+    setAllChecked(!allChecked);
+  }
+
+  function restoreTodo(todo: Todo) {
+    setTodos(todos => {
+      return [...todos, { ...todo, completed: false }];
+    });
+
+    setTodoHistory(currentHistory => {
+      return [...currentHistory.filter(curr => curr.id !== todo.id)];
+    })
+  }
+
+  function toggleShowHistory() {
+    setShowHistory(!showHistory)
+  }
+
+  function clearTodoHistory() {
+    setTodoHistory([]);
+
+    localStorage.removeItem("TODO_HISTORY");
   }
 
   return (
     <>
-      {todos.map(todo => <Todo key={todo} text={todo} handleRemove={removeTodo}/>)}
-      <br/>
-      <input type="text" ref={input}></input>
-      <button onClick={handleClick}>Add Todo</button>
+      <NewTodoForm onSubmit={addTodo} />
+      <h1>Todo List</h1>
+      <TodoList
+        todos={todos}
+        deleteTodo={deleteTodo}
+        toggleTodo={toggleTodo}
+      />
+      <br />
+      <TodoButtons
+        toggleShowHistory={toggleShowHistory}
+        deleteMarkedTodos={deleteMarkedTodos}
+        allChecked={allChecked}
+        checkAllTodos={checkAllTodos}
+      />
+      {showHistory &&
+        <TodoHistory
+          todoHistory={todoHistory}
+          restoreTodo={restoreTodo}
+          clearHistory={clearTodoHistory}
+        />}
     </>
-  )
+  );
 }
 
 export default App;
